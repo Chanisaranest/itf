9bea7e86d646f1a5dd88c8b367c6a20768ffc4de
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2020-11-17 22:44:30 +0700

    Update show.php

--- show.php
+++ show.php
@@ -16,7 +16,7 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
   <tr>
     <th width="100" style="background-color : B6E3E9;"> <div align="center">Name</div></th>
     <th width="350" style="background-color : C3DDE0;"> <div align="center">Comment </div></th>
-    <th width="150" style="background-color : 7CCED1;"> <div align="center">Link </div></th>
+    <th width="150" style="background-color : 7CCED1;"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -25,12 +25,13 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td style="background-color: C4E4F4;"><?php echo $Result['Name'];?></div></td>
     <td style="background-color: ABBDEE;"><?php echo $Result['Comment'];?></td>
-    <td style="background-color: E7D2EF;"><?php echo $Result['Link'];?></td>
+    <td style="background-color: E7D2EF;"><input type="button" value="Delete" style="background-color: FAAE9F;"><input type="button" value="Edit" style="background-color: FAAE9F;"></td>
   </tr>
 <?php
 }
 ?>
 </table>
+<input type="button" value="ADD" style="background-color: FAAE9F;">
 <?php
 mysqli_close($conn);
 ?>

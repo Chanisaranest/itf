e178eee93598d17c3a3144efb6239a8c8185aa9f
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2020-11-17 22:55:43 +0700

    Update show.php

--- show.php
+++ show.php
@@ -31,7 +31,7 @@ while($Result = mysqli_fetch_array($res))
 }
 ?>
 </table>
-<input type="button" value="ADD" style="background-color: FAAE9F;margin-left: 300px;">
+<input type="button" value="ADD" style="background-color: FAAE9F;margin-left: 300px;" onclick="document.location='form.html'">
 <?php
 mysqli_close($conn);
 ?>

9825907a3eea24f6c5998d7204dd2d309e8bfde7
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2020-11-18 23:26:25 +0700

    Update show.php

--- show.php
+++ show.php
@@ -14,10 +14,10 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table style="padding-top: 50px; padding-left: 100px;" width="600" border="1">
   <tr>
-    <th width="100" style="background-color : B6E3E9;"> <div align="center">Name</div></th>
+    <th width="250" style="background-color : B6E3E9;"> <div align="center">Name</div></th>
     <th width="350" style="background-color : C3DDE0;"> <div align="center">Comment </div></th>
-    <th width="150" style="background-color : 88DDBB;"> <div align="center">Link </div></th>
-    <th width="150" style="background-color : 7CCED1;"> <div align="center">Action </div></th>
+    <th width="300" style="background-color : 88DDBB;"> <div align="center">Link </div></th>
+    <th width="250" style="background-color : 7CCED1;"> <div align="center">Action </div></th>
   </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
@@ -27,7 +27,7 @@ while($Result = mysqli_fetch_array($res))
     <td style="background-color: C4E4F4;"><?php echo $Result['Name'];?></div></td>
     <td style="background-color: ABBDEE;"><?php echo $Result['Comment'];?></td>
     <td style="background-color: B9E3AE;"><?php echo $Result['Link'];?></td>
-    <td style="background-color: E7D2EF;"><input type="button" value="Delete" style="background-color: FAAE9F;"><input type="button" value="Edit" style="background-color: FAAE9F;"></td>
+    <td style="background-color: E7D2EF;"><button type ='submit' style="background-color:FAAE9F;"><a href="delete.php?id=<?=$Result['id'];?>">Detele</a></button></td>
   </tr>
 <?php
 }

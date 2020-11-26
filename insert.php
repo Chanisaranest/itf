dbb386e7dc5d0792e9d09787c8ff4b1516bcbaa1
Chanisaranest <73680459+Chanisaranest@users.noreply.github.com>
2020-11-26 22:42:11 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -24,3 +24,13 @@ if (mysqli_query($conn, $sql)) {
   
 mysqli_close($conn);
 ?>
+<!-- <!DOCTYPE html>
+<html>
+        <body>
+            <form action ="welcome.php" method="POST">
+                Name: <input type="text" name="name" /><br>
+                Age: <input type="text" name="age" /> <br>
+                <input type="submit" />
+            </form>
+        </body>
+</html> -->
